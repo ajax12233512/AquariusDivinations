9238cffc21282b1517a19736c332e220a0a147d4
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2023-07-03 19:39:09 -0500

    Add project files.

---
+++ AquariusDivinations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AquariusDivinations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AquariusDivinations/wwwroot/js/nine-card-spread.js
@@ -0,0 +1,13 @@
+﻿var cardImages = [
+    "~/cardimgs/ace of cups.png",
+    "~/cardimgs/ace of pentacles",
+    // Add the paths for card3 to card9
+];
+
+$(document).ready(function () {
+    $(".card").each(function (index) {
+        var imagePath = cardImages[index];
+        $(this).css("background-image", "url(" + imagePath + ")");
+    });
+});
+
